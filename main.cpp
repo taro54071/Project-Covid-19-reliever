3eb9fe6ab1b159f0f7d53692d335bee7de0a03f9
Wasin Phaijith <momo.taro9071@gmail.com>
2021-05-09 03:40:34 +0700

    First commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "i here auan";
+    cout << "kuy pun na hee kys na e dok :DDD";
+}
+// กุ set path แปป
+// มึงมาคุยในแชทมา
+// ขก สลับไปdiscordบ่อย
+// this is fine
+// this is not fine
